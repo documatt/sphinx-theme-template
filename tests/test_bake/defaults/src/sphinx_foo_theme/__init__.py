71e434269b530de70b5b90f7922f589baf65fbe7
Libor Jelínek <liborjelinek@documatt.com>
2025-04-09 15:49:18 +0200

    feat: version and description of generated project is dynamic field of pyproject.toml

--- tests/test_bake/defaults/src/sphinx_foo_theme/__init__.py
+++ tests/test_bake/defaults/src/sphinx_foo_theme/__init__.py
@@ -1,17 +1,25 @@
 """
-Register theme with Sphinx.
+Foo but full-bar theme.
 """
+
+__version__ = "0.1.0"
+
 
 from pathlib import Path
 
 from sphinx.application import Sphinx
+from sphinx.util.typing import ExtensionMetadata
 
 THEME_NAME = "sphinx_foo_theme"
 
 
-def setup(app: Sphinx) -> dict[str, bool]:
+def setup(app: Sphinx) -> ExtensionMetadata:
     """Setup the Sphinx application."""
     theme_path = str(Path(__file__).parent.resolve())
     app.add_html_theme(THEME_NAME, theme_path)
 
-    return {"parallel_read_safe": True, "parallel_write_safe": True}
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+        "version": __version__,
+    }
